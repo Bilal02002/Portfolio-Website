7b78ba9c83eb9cb8774876532fd537d5e3ffc794
Bilal02002 <bilalahsanchaudhary@gmail.com>
2022-06-17 17:28:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function a()
+{
+     var x = prompt("Are You Student ?")
+    if(x=="yes")
+    {
+        let a="In Progress ^_^";
+      alert(a);
+    }
+    else
+    {
+        let b="Will be available soon...!";
+     alert(b);
+    }
+} 
